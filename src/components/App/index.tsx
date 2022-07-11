bc3514b49d0a6004ade19caa9a75c7b3033cd102
stwgabriel <gabrielstw@pm.me>
2022-07-11 14:08:03 -0300

    chore: define App as a component

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import Routes from '../../routes';
+import GlobalStyles from '../../styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
