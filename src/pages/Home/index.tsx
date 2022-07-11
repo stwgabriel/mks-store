445f662418dc356ed110a26e2a32d72224d10c1a
stwgabriel <gabrielstw@pm.me>
2022-07-11 14:10:44 -0300

    feat: create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import { HomeContainer } from './homeStyles';
+
+// icons
+import cartIcon from '../../assets/icons/cart.svg';
+import Product from '../../components/Product';
+
+function Home() {
+
+  return (
+    <HomeContainer>
+      <header>
+
+        <h1 className="page-logo">
+          MKS
+          {' '}
+          <span>Store</span>
+        </h1>
+
+        <button
+          type="button"
+          className="cart-button"
+        >
+          <img src={cartIcon} alt="cart" />
+        </button>
+      </header>
+
+      <main>
+        <ul className="products-list">
+          <Product
+            title="Apple iPhone X 128GB"
+            price={899.00}
+            description="Redesigned from scratch and completely revised."
+            imagePath="/iphone-x.png"
+          />
+        </ul>
+      </main>
+    </HomeContainer>
+  );
+}
+
+export default Home;
